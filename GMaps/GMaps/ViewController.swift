00988066f13fe136471122fdfc0a6f5751adb284
chenchangqing <chenchangqing198@126.com>
2015-10-31 16:34:38 +0800

    2015.10.31 tempci

---
+++ GMaps/GMaps/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  GMapsDemo
+//
+//  Created by Gabriel Theodoropoulos on 29/3/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var viewMap: GMSMapView!
+    
+    @IBOutlet weak var bbFindAddress: UIBarButtonItem!
+    
+    @IBOutlet weak var lblInfo: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(48.857165, longitude: 2.354613, zoom: 8.0)
+        viewMap.camera = camera
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    // MARK: IBAction method implementation
+    
+    @IBAction func changeMapType(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func findAddress(sender: AnyObject) {
+    
+    }
+    
+    
+    @IBAction func createRoute(sender: AnyObject) {
+    
+    }
+    
+    
+    @IBAction func changeTravelMode(sender: AnyObject) {
+    
+    }
+    
+    
+}
+
